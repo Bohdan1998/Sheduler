dc5efeed79c82933e31408e5458849304a2f731f
Legion <slavik.shagin@gmail.com>
2020-03-23 11:43:35 +0200

    add assets, drawer&other components

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import firebase from './src/config'
+
+function storeHighScore(userId, score) {
+  firebase.database().ref('users/' + userId).set({
+    highscore: score
+  });
+}
 
 export default function App() {
+
+  
+
+  //  test button (place into return)
+  //  <Button title='write' onPress={()=>storeHighScore(3245673,324534)} />
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      
     </View>
   );
 }
